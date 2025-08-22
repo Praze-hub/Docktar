aa2a35939369d3cf2baa5a60ae696c66621205f6
Praze-hub <godspraiseokechukwu07@gmail.com>
2025-08-22 08:02:32 +0100

    Changed rest framework config

--- core/urls.py
+++ core/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/chatbot/', include('chatbot.urls')),
 
 ]
